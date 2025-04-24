6a55087e2f02b3076b0c5c00c8692444174c07d9
LuongTran <tranluong025@gmail.com>
2025-04-24 20:24:12 +0700

    Clean up generated starter code

--- composeApp/src/commonMain/kotlin/io/github/tranhuuluong/kmpgithubclient/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package io.github.tranhuuluong.kmpgithubclient
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/io/github/tranhuuluong/kmpgithubclient/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.github.tranhuuluong.kmpgithubclient
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
